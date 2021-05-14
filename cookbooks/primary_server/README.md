725e9ae2341b8530de8f63ce8ae11bb25649a402
Tarak311 <tarak_1@live.com>
2021-05-14 11:59:11 -0400

    commit

---
+++ cookbooks/primary_server/README.md
@@ -0,0 +1,4 @@
+# primary_server
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/primary_server/recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: primary_server
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+#include_recipe 'os-hardening::default'
