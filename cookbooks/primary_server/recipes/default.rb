aa2a005317a12eb113734775f85d8b38f4f3f069
Tarak311 <tarak_1@live.com>
2021-05-16 15:27:36 -0400

    master

--- cookbooks/primary_server/recipes/default.rb
+++ cookbooks/primary_server/recipes/default.rb
@@ -5,3 +5,10 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 include_recipe 'os-hardening::default'
 include_recipe 'chef-client::default'
+
+
+dnf_package 'base-tools' do
+    flush_cache [ :after ]
+    package_name      %w(net-tools python3 NetworkManager-wifi)
+    action            :upgrade # defaults to :install if not specified
+  end
